20dee696582522958a115f57b0a4f30ac97e9f5d
rl627 <rl627@drexel.edu>
2026-01-12 14:50:25 -0500

    restored __version__ in root init.py

--- src/assert_polars/__init__.py
+++ src/assert_polars/__init__.py
@@ -4,4 +4,5 @@ from assert_polars.core import hello, verify
 from assert_polars.errors import CheckError
 from assert_polars.predicates import is_uniq
 
+__version__ = "0.2.1"
 __all__ = ["hello", "verify", "CheckError", "is_uniq", "__version__"]
